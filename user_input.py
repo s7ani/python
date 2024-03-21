e02c7144992692994b659a15f07858566d2cb733
s7ani <lazaroshani78@gmail.com>
2024-03-21 05:42:24 -0700

    user input

---
+++ user_input.py
@@ -0,0 +1,4 @@
+name=input("enter your name: ")
+age=int(input("enter your age: "))
+location=input("enter your location: ")
+print ("hello " +name+",you are "+str(age) +" years old and live in " +location)
